a6ffb7bb4f4aa11a1363d0a23984114da20618e7
Hyunho Son <engel0320@gmail.com>
2021-05-31 20:10:51 +0900

    for msa

--- Order/src/main/java/onlinebookstore/external/BookService.java
+++ Order/src/main/java/onlinebookstore/external/BookService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@FeignClient(name="Book", url="http://localhost:8082")
+@FeignClient(name="Book", url="http://Book:8082")
 public interface BookService {
 
     @RequestMapping(method= RequestMethod.GET, path="/books/chkAndModifyStock")
