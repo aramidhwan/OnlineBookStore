67b089f93efa05a61a7d756ba41959eacfc6ded3
Hyunho Son <engel0320@gmail.com>
2021-06-02 02:36:13 +0900

    order - book target url : configmap

--- Order/src/main/java/onlinebookstore/external/BookService.java
+++ Order/src/main/java/onlinebookstore/external/BookService.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@FeignClient(name="Book", url="http://Book:8080")
+//@FeignClient(name="Book", url="http://Book:8080")
+@FeignClient(name="Book", url="${api.url.book}")
 public interface BookService {
 
     @RequestMapping(method= RequestMethod.GET, path="/books/chkAndModifyStock")
